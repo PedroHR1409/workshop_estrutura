43c190b3650f1ddf61c47b4dedfcae258e0ce281
Pedro <pedro.h.roque14@gmail.com>
2025-04-04 20:45:44 -0300

    feat: adicionada etapa de extração

---
+++ src/pipeline/extract.py
@@ -0,0 +1,24 @@
+import os
+import glob
+from typing import List
+
+import pandas as pd
+
+def extract_files(folder_path: str) -> List[pd.DataFrame]:
+    """
+    Função para ler os arquivos de uma pasta data/input
+    e retornar uma lista de dataframes
+    
+    args: folder_path (str): Caminho da pasta com arquivos
+
+    returns: Lista de dataframes
+    
+    """
+    all_files = glob.glob(os.path.join(folder_path, "*.xlsx"))
+
+    dataframe_list = []
+
+    for file in all_files:
+        dataframe_list.append(pd.read_excel(file))
+
+    return dataframe_list
