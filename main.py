fb9b314a37c2d21cda62e190d4ab6e022720b9e4
Pedro <pedro.h.roque14@gmail.com>
2025-04-05 15:06:16 -0300

    feat: função principal de execução do projeto

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
+"""
+Este módulo executa o pipeline de processamento de dados.
+
+Carregando os dados da pasta de entrada e gerando um arquivo consolidado
+na pasta de saída.
+
+As pastas de entrada e saída e o nome do arquivo de saída são definidos
+como variáveis no código.
+O processo é realizado pela função 'pipeline_completa', que é chamada dentro
+do bloco principal.
+
+Dependências:
+- src.pipeline.pipeline: Função pipeline_completa
+"""
 from src.pipeline.pipeline import pipeline_completa
 
+"""Arquivo onde o projeto é executado"""
 if __name__ == '__main__':
+    """Função de execução do projeto"""
     input_folder = 'data/input'
     output_folder = 'data/output'
     output_file_name = 'consolidate_data.xlsx'
