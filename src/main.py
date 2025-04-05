ddc9b31a1910197851c332d42a77395907dd507d
Pedro <pedro.h.roque14@gmail.com>
2025-04-04 21:37:19 -0300

    feat: adicionado funções para fazer o load

--- src/main.py
+++ src/main.py
@@ -1,6 +1,9 @@
 from pipeline.extract import extract_files
+from pipeline.load import save_df
 from pipeline.transform import concat_from_list
 
-dfs = extract_files('./data/input')
-df_merged = concat_from_list(dfs)
-print(df_merged)
+if __name__ == '__main__':
+    dfs = extract_files('./data/input')
+    df_merged = concat_from_list(dfs)
+    
+    save_df(df_merged, 'data/output/consolidated_data.xlsx')
--- src/pipeline/load.py
+++ src/pipeline/load.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-def save_df(df: pd.DataFrame, folder: str) -> None:
+def save_df(df: pd.DataFrame, folder: str, file_name: str) -> None:
     """
     Função para salvar o DataFrame no caminho informado
     """
-    return df.to_excel(folder, index=False, header=True)
+    df.to_excel(f'{folder}/{file_name}.xlsx', index=False)
