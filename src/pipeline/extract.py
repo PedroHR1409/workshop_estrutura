283cd3a3fa7e75752d22b59bc589625cba5edbaa
Pedro <pedro.h.roque14@gmail.com>
2025-04-05 15:07:43 -0300

    feat: adicionado documentação e pep8

--- src/pipeline/extract.py
+++ src/pipeline/extract.py
@@ -1,18 +1,33 @@
+"""
+Este módulo contém funções para extrair dados de arquivos Excel.
+
+A função principal, `extract_files`, lê os arquivos `.xlsx` da pasta fornecida
+e retorna uma lista de DataFrames.
+
+Funções:
+- extract_files(folder_path: str) -> List[pd.DataFrame]: Lê todos os arquivos
+Excel em uma pasta e retorna uma lista de DataFrames.
+"""
+
 import glob
 import os
 from typing import List
 
 import pandas as pd
 
+"""Funções para extrair dados"""
+
 
 def extract_files(folder_path: str) -> List[pd.DataFrame]:
     """
-    Função para ler os arquivos de uma pasta data/input
-    e retornar uma lista de dataframes
+    Função para ler arquivos de uma pasta e retornar uma lista de dataframes.
 
-    args: folder_path (str): Caminho da pasta com arquivos
+    args:
+        folder_path (str): Caminho da pasta com arquivos
 
-    returns: Lista de dataframes
+    returns:
+        List[pd.DataFrame]: Lista de dataframes lidos dos
+        arquivos Excel
 
     """
     all_files = glob.glob(os.path.join(folder_path, '*.xlsx'))
